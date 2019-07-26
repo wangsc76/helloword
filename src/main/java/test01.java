a3182740f14efa1daf583668290398f217b98ab6
kuangwei <19976139162@qq.com>
2019-07-26 18:45:50 +0800

    测试2！！！

---
+++ src/main/java/test01.java
@@ -0,0 +1,10 @@
+/**
+ * @author Lenovo
+ * @version v 1.0
+ * @date 2019/7/26
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("第一次测试开始");
+    }
+}
